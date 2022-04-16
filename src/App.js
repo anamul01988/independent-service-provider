69c3fe6aa9bfd27c88e6933bdec3075ccf82e7bd
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-04-16 23:28:11 +0600

    header component done with active route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Shared/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Blog from './components/Blog/Blog';
+import AboutMe from './components/AboutMe/AboutMe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <Header/>
+        <Routes>
+            <Route path="/" element = {<Home/>} />
+            <Route path="/blog" element = {<Blog/>} />
+            <Route path="/about" element = {<AboutMe/>} />
+        </Routes>
     </div>
   );
 }
---
+++ src/components/Shared/Header/Header.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import './Header.css';
+import logo from '../../../asset/logo.png';
+// import { Container, Nav, Navbar, NavDropdown, NavLink } from "react-bootstrap";
+
+const Header = () => {
+    return (
+        <div className="container mt-2">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+              <NavLink to="/navbar" >
+                  <img className="logo link" src={logo} alt="logo"  /> 
+                  <span className=" title">Medical Informatics</span>
+                 
+              </NavLink>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarTogglerDemo02"
+                aria-controls="navbarTogglerDemo02"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav nav-responsive ms-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <NavLink to="/" className={({ isActive }) => (isActive ? "active-link" : "link" )} aria-current="page" >
+                      Home
+                    </NavLink>
+                  </li>
+                
+                  <li className="nav-item">
+                    <NavLink to="/blog" className={({ isActive }) => (isActive ? "active-link" : "link" )}>Blog</NavLink>
+                  </li>
+       
+                  <li className="nav-item">
+                    <NavLink to="/about" className={({ isActive }) => (isActive ? "active-link" : "link" )}>About Me</NavLink>
+                  </li>
+                </ul>
+               
+              </div>
+            </div>
+          </nav>
+        </div>
+      );
+};
+
+export default Header;
