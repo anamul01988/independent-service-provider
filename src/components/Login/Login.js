a6ace8b43b038bb8eebcb73cd4d2bdf97dc7a75b
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-04-18 01:14:18 +0600

    login authentication is done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Login = () => {
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const navigate = useNavigate();
+
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  if(user){
+     navigate('/');
+  }
+
+  const handleSubmit = event =>{
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    // console.log(email, password)
+    signInWithEmailAndPassword(email, password)
+
+  }
   return (
     <div className="container my-5 ">
       <h3 className="text-secondary text-center mt-3">Please login.</h3>
-      <Form className="w-50 mx-auto">
+      <Form onSubmit={handleSubmit} className="w-50 mx-auto">
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Control ref={emailRef} type="email" placeholder="name@example.com" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="password" />
+          <Form.Control ref={passwordRef} type="password" placeholder="password" />
         </Form.Group>
         <Button  className="w-50 mx-auto d-block my-4 bg-warning " type="submit">
           Login
