82542768a3d7368f0d57c3e4be0367173d5ca876
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-04-18 17:25:45 +0600

    about me component added

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,11 +1,22 @@
-import React from 'react';
-
+import React from "react";
+import './AboutMe.css';
+import { Button, Card } from "react-bootstrap";
+import pic from '../../asset/about/main_pic.jpg';
 const AboutMe = () => {
-    return (
-        <div>
-            <h2>this is about me component.</h2>
-        </div>
-    );
+  return (
+    <div className="container about d-flex justify-content-center my-5 py-5">
+      <Card style={{ width: "20rem" }} className="shadow rounded p-3">
+        <Card.Img className="img" variant="top" src={pic}  />
+        <Card.Body>
+          <Card.Title>My Goal</Card.Title>
+          <Card.Text>
+            I would like to be a full Stack Web Developer. Now i am working with front end developer on react. I just trying to boost my skill on programming. I have been working hard so that i can be a entrepreneur in It sector.
+          </Card.Text>
+          <Button variant="secondary">More Details</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default AboutMe;
